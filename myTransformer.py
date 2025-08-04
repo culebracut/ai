d3fd5679ad59fbbfd2a32b571b821f9fa900fe03
culebracut <foobar.warren@gmail.com>
2025-08-04 16:36:51 -0700

    foo

--- myTransformer.py
+++ myTransformer.py
@@ -6,11 +6,14 @@ chat = [
     {"role": "user", "content": "Hey, can you tell me any fun things to do in New York?"}
 ]
 
-# pipeline = pipeline(task="text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct", torch_dtype=torch.bfloat16, device_map="auto")
-# response = pipeline(chat, max_new_tokens=512)
-# print(response[0]["generated_text"][-1]["content"])
+pipeline = pipeline(task="text-generation", model="meta-llama/Meta-Llama-3-8B-Instruct", torch_dtype=torch.bfloat16, device_map="auto")
+response = pipeline(chat, max_new_tokens=512)
+print(response[0]["generated_text"][-1]["content"])
 
+# pipeline = pipeline(task="text-generation", model="Qwen/Qwen2.5-1.5B")
+# pipeline("the secret to baking a really good cake is ")
+# [{'generated_text': 'the secret to baking a really good cake is 1) to use the right ingredients and 2) to follow the recipe exactly. the recipe for the cake is as follows: 1 cup of sugar, 1 cup of flour, 1 cup of milk, 1 cup of butter, 1 cup of eggs, 1 cup of chocolate chips. if you want to make 2 cakes, how much sugar do you need? To make 2 cakes, you will need 2 cups of sugar.'}]
 
-pipeline = pipeline(task="automatic-speech-recognition", model="openai/whisper-large-v3")
-pipeline("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
-{'text': ' I have a dream that one day this nation will rise up and live out the true meaning of its creed.'}
+# pipeline = pipeline(task="automatic-speech-recognition", model="openai/whisper-large-v3")
+# pipeline("https://huggingface.co/datasets/Narsil/asr_dummy/resolve/main/mlk.flac")
+# {'text': ' I have a dream that one day this nation will rise up and live out the true meaning of its creed.'}
